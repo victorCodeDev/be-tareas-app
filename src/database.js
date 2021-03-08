585f52bb8e3950e10003a4a39a243cfcf61982d2
victor.ojeda <victor.ojeda@konecta.com.py>
2021-03-08 17:49:41 -0300

    Realización de backend para aplicación de tareas.  Se realizó api  para un listado de tareas públicas y privadas consumiento los datos de un json Se validó que para acceder a tareas privadas se encuentre logeado.

---
+++ src/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/angular-auth', {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+    .then(db => console.log('Database is Connected'))
+    .catch(err => console.log(err))
