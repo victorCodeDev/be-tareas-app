e458b8a9a16eddfe61e786773eef872bbd2a7caa
vojeda <vojeda@set.gov.py>
2022-10-21 09:01:06 -0300

    se remueven console logs y se formatea código en general

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,102 +1,102 @@
-const { Router} = require('express');
+const { Router } = require('express');
 const router = Router();
 
 const User = require('../models/User');
 
 const jwt = require('jsonwebtoken');
 
-router.post('/signup', async (req, res) =>{
-    
-    const {email, password} = req.body;
-    const newUser = new User({email, password});
-    await newUser.save();
+router.post('/signup', async (req, res) => {
 
-    const token = jwt.sign({id:newUser.id}, 'secretkey');
+  const { email, password } = req.body;
+  const newUser = new User({ email, password });
+  await newUser.save();
 
-    res.status(200).json({token});
-    
+  const token = jwt.sign({ id: newUser.id }, 'secretkey');
+
+  res.status(200).json({ token });
+
 
 })
 
 
-router.post('/signin', async (req, res) =>{
-    
-    const {email, password} = req.body;
-    const user = await User.findOne({email})
+router.post('/signin', async (req, res) => {
 
-   if (!user) return res.status(401).send("El email no existe");
-   if (user.password !== password) return res.status(401).send("Contraseña incorrecta");
-   
-   const token = jwt.sign({id:user.id}, "secretkey");
+  const { email, password } = req.body;
+  const user = await User.findOne({ email })
 
-   res.status(200).json({token});
-    
+  if (!user) return res.status(401).send("El email no existe");
+  if (user.password !== password) return res.status(401).send("Contraseña incorrecta");
+
+  const token = jwt.sign({ id: user.id }, "secretkey");
+
+  res.status(200).json({ token });
+
 
 })
 
 router.get('/tasks', (req, res) => {
-    res.json([
-       {
-        id: 1,
-        name: 'TAREA 1',
-        description: 'DESCRIPCION PARA TAREA 1',
-        date: "2020-03-13T15:54:51.451Z"
-       },
-       {
-        id: 2,
-        name: 'TAREA 2',
-        description: 'DESCRIPCION PARA TAREA 2',
-        date: "2020-03-13T15:54:51.451Z"
-       },
-       {
-        id: 3,
-        name: 'TAREA 3',
-        description: 'DESCRIPCION PARA TAREA 3',
-        date: "2020-03-13T15:54:51.451Z"
-       }
-        
-    ])
+  res.json([
+    {
+      id: 1,
+      name: 'TAREA 1',
+      description: 'DESCRIPCION PARA TAREA 1',
+      date: "2020-03-13T15:54:51.451Z"
+    },
+    {
+      id: 2,
+      name: 'TAREA 2',
+      description: 'DESCRIPCION PARA TAREA 2',
+      date: "2020-03-13T15:54:51.451Z"
+    },
+    {
+      id: 3,
+      name: 'TAREA 3',
+      description: 'DESCRIPCION PARA TAREA 3',
+      date: "2020-03-13T15:54:51.451Z"
+    }
+
+  ])
 })
 
 
 router.get('/private-tasks', verifyToken, (req, res) => {
-    res.json([
-       {
-        id: 1,
-        name: 'TAREA PRIVADA 1',
-        description: 'DESCRIPCION PARA TAREA PRIVADA 1',
-        date: "2020-03-13T15:54:51.451Z"
-       },
-       {
-        id: 2,
-        name: 'TAREA PRIVADA 2',
-        description: 'DESCRIPCION PARA TAREA PRIVADA 2',
-        date: "2020-03-13T15:54:51.451Z"
-       },
-       {
-        id: 3,
-        name: 'TAREA PRIVADA 3',
-        description: 'DESCRIPCION PARA TAREA PRIVADA 3',
-        date: "2020-03-13T15:54:51.451Z"
-       }
-        
-    ])
+  res.json([
+    {
+      id: 1,
+      name: 'TAREA PRIVADA 1',
+      description: 'DESCRIPCION PARA TAREA PRIVADA 1',
+      date: "2020-03-13T15:54:51.451Z"
+    },
+    {
+      id: 2,
+      name: 'TAREA PRIVADA 2',
+      description: 'DESCRIPCION PARA TAREA PRIVADA 2',
+      date: "2020-03-13T15:54:51.451Z"
+    },
+    {
+      id: 3,
+      name: 'TAREA PRIVADA 3',
+      description: 'DESCRIPCION PARA TAREA PRIVADA 3',
+      date: "2020-03-13T15:54:51.451Z"
+    }
+
+  ])
 })
 
-function verifyToken(req, res, next){
-    if (!req.headers.authorization){
-        res.status(401).send("Sin autorizacion");
-    }
+function verifyToken(req, res, next) {
+  if (!req.headers.authorization) {
+    res.status(401).send("Sin autorizacion");
+  }
 
-    const token = req.headers.authorization.split(' ')[1];
+  const token = req.headers.authorization.split(' ')[1];
 
-    if (token === 'null'){
-        return res.status(401).send("Sin autorizacion");
-    }
+  if (token === 'null') {
+    return res.status(401).send("Sin autorizacion");
+  }
 
-    const payload = jwt.verify(token, 'secretkey');
-    req.userId = payload.id;
-    next();
+  const payload = jwt.verify(token, 'secretkey');
+  req.userId = payload.id;
+  next();
 
 
 
